4004fbae548c95a277934cd535579b3ed113eedf
qunqunstyle <335394179@qq.com>
2018-09-16 16:23:41 +0800

    daily submit

---
+++ C++机试题/京东/中位数.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cassert>
+#include <stack>
+#include <math.h>
+#include<algorithm>
+using namespace std;
+
+bool cmp(int a,int b){
+	return  a<b;
+}
+
+float EvaluateMedian(int a[], int n)
+{
+	sort(a,a+n,cmp);
+	if(n % 2 !=0)
+	{
+		return a[n / 2];
+	}
+	else
+	{
+		return ((float)(a[n / 2] + a[n / 2 -1]))/ 2;
+	}
+}
+ 
+int main()
+{
+	int num ;
+	cin>>num;
+	int a[100]; 
+	for(int i=0;i<num;i++){
+		cin>>a[i];
+	}
+
+	cout<<EvaluateMedian(a, num)<<endl;
+	
+ 
+	return 0;
+}
